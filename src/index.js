bcc7a17c1287ad4306b8530b788e8061fddb7f20
Njalale <fnjalale@gmail.com>
2023-03-23 00:06:03 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const changeId = document.getElementById("new-task-description");
+  changeId.id = "task"
+
+  function handleTask(tasks){
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x'
+    p.textContent = `${tasks} `
+    p.appendChild(btn)
+    console.log(p);
+    document.querySelector('#list').appendChild(p)
+  }
+  
+  function handleDelete(event){
+    event.target.parentNode.remove()
+  }
+
+   let form = document.querySelector('form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    handleTask(event.target.task.value);
+    form.reset()
+  })
+
 });
+
